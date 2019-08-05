d93d900d6266b3c3ae6b137686ce77460a0ef71a
Radek Mezulanik <radek@productboard.com>
2019-08-05 11:26:14 +0200

    react-app in folder

---
+++ twentynineteen/myReactApp/build/precache-manifest.ece20369d2b0960b7fc992bf621ae86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb0eb75bf8e11f30190cb35d1e39b6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e773b38ac31f4c2acfd4",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "e773b38ac31f4c2acfd4",
+    "url": "/static/js/main.52e5670d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
